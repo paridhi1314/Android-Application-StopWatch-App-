f7fb80fd8261ba0554f594d606afbe4f06d9c695
Paridhi Malav <malavparidhi1306@gmail.com>
2021-05-29 12:14:25 +0530

    StopWatch App

---
+++ app/src/main/java/com/example/stopwatch/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.stopwatch;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    TextView tvSplash, tvSubSplash;
+    Button btnget;
+
+    Animation atg, btgone, btgtwo;
+    ImageView ivSplash;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tvSplash = findViewById(R.id.tvSplash);
+        tvSubSplash = findViewById(R.id.tvSubSplash);
+        btnget = findViewById(R.id.btnget);
+        ivSplash = findViewById(R.id.ivSplash);
+
+        //load animation
+        atg = AnimationUtils.loadAnimation(this,R.anim.atg);
+        btgone = AnimationUtils.loadAnimation(this,R.anim.btgone);
+        btgtwo = AnimationUtils.loadAnimation(this,R.anim.btgtwo);
+
+        //passing animation
+        ivSplash.startAnimation(atg);
+        tvSplash.startAnimation(btgone);
+        tvSubSplash.startAnimation(btgone);
+        btnget.startAnimation(btgtwo);
+        
+//        ActionBar actionBar = getSupportActionBar();
+//        actionBar.hide();
+    }
+}
